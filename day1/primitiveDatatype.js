e4987bf2111deee944df24118468b998175295a7
neethuneets425-stack <neethuneets425@gmail.com>
2025-08-16 13:15:31 +0530

    Add files via upload

---
+++ day1/primitiveDatatype.js
@@ -0,0 +1,10 @@
+var firstName = "Neethu"
+var companyName = "Infosys"
+var mobileNumber = "9384103555"
+var isAutomation = "true"
+var hasPlaywright
+console.log(typeof firstName)
+console.log(typeof companyName)
+console.log(typeof mobileNumber)
+console.log(typeof isAutomation)
+console.log(typeof hasPlaywright)
