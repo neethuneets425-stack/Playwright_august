7bc09df0054cac117f1f0b1662dfaa91ee94b620
neethuneets425-stack <neethuneets425@gmail.com>
2025-08-17 08:37:14 +0530

    Add files via upload

---
+++ homeassignment.js
@@ -0,0 +1,21 @@
+var city = "Palakkad"
+console.log("I live in " +city)
+console.log(typeof city)
+let marks = "85"
+console.log(marks+10)
+console.log(typeof marks)
+const isWeekend = "true"
+if (isWeekend)
+    {
+    console.log("relax")
+    }
+    else
+    {
+     console.log("work")   
+    }
+ console.log(typeof isWeekend) 
+ var futureGoal
+ console.log(futureGoal)
+ console.log(typeof futureGoal)  
+
+
