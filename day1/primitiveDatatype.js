906aed30709352f94e67574781beb9b98c2e3f8c
neethuneets425-stack <neethuneets425@gmail.com>
2025-08-17 08:47:24 +0530

    Update primitiveDatatype.js

--- day1/primitiveDatatype.js
+++ day1/primitiveDatatype.js
@@ -1,10 +1,11 @@
 var firstName = "Neethu"
 var companyName = "Infosys"
 var mobileNumber = "9384103555"
-var isAutomation = "true"
+var isAutomation = true
 var hasPlaywright
 console.log(typeof firstName)
 console.log(typeof companyName)
 console.log(typeof mobileNumber)
 console.log(typeof isAutomation)
+
 console.log(typeof hasPlaywright)
